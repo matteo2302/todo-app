b5664cc378e3c356f3b311e1c9fae52a69194fb6
matteo2302 <matteo.pizzichemi01@gmail.com>
2025-08-08 15:37:02 +0200

    to do list react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Todoform from './Todoform';
+import { useState } from 'react';
 
 function App() {
+
+  let [task, setTask] = useState(["prima task"]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>TODO LIST</h1>
+    <Todoform />
+     <p>{task}</p>
     </div>
   );
 }
---
+++ src/Todoform.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+function Todoform() {
+let [newTask, setNewTask] = useState("");
+const handleSubmit = (e) => {
+  e.preventDefault();
+  if(newTask.trim() !== ""){
+    console.log(newTask);
+    setNewTask("");
+  }};
+
+
+
+
+
+  return (
+      <form onSubmit={handleSubmit}>
+      <input onChange={(e)=>setNewTask(e.target.value)} value={newTask}  type="text" placeholder="nuova task" />
+      <button type='submit'>aggiungi</button>
+      
+      </form>
+    );
+};
+export default Todoform;
